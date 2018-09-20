e668ba6ed2cdde3c99c50ee1f3a66d8ba4f9db03
Zavvio Mok <zavvio@gmail.com>
2018-09-20 15:08:40 -0700

    Small Triangles, Large Triangles

--- small-triangles-large-triangles.c
+++ small-triangles-large-triangles.c
@@ -1,4 +1,16 @@
 // https://www.hackerrank.com/challenges/small-triangles-large-triangles
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+struct triangle
+{
+	int a;
+	int b;
+	int c;
+};
+
+typedef struct triangle triangle;
 
 double calc_area(int a, int b, int c) {
     double p = 1.0 * (a + b + c) / 2;
@@ -22,4 +34,19 @@ void sort_by_area(triangle* tr, int n) {
             }
         }
     }
+}
+
+int main()
+{
+	int n;
+	scanf("%d", &n);
+	triangle *tr = malloc(n * sizeof(triangle));
+	for (int i = 0; i < n; i++) {
+		scanf("%d%d%d", &tr[i].a, &tr[i].b, &tr[i].c);
+	}
+	sort_by_area(tr, n);
+	for (int i = 0; i < n; i++) {
+		printf("%d %d %d\n", tr[i].a, tr[i].b, tr[i].c);
+	}
+	return 0;
 }
