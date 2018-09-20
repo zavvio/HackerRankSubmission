c7d158209754159e8eddc0044ea46ae904eb51c7
Zavvio Mok <zavvio@gmail.com>
2018-09-20 15:10:45 -0700

    Permutations of Strings

--- permutations-of-strings.c
+++ permutations-of-strings.c
@@ -46,3 +46,25 @@ int next_permutation(int n, char **s)
     }
     return 1;
 }
+
+int main()
+{
+	char **s;
+	int n;
+	scanf("%d", &n);
+	s = calloc(n, sizeof(char*));
+	for (int i = 0; i < n; i++)
+	{
+		s[i] = calloc(11, sizeof(char));
+		scanf("%s", s[i]);
+	}
+	do
+	{
+		for (int i = 0; i < n; i++)
+			printf("%s%c", s[i], i == n - 1 ? '\n' : ' ');
+	} while (next_permutation(n, s));
+	for (int i = 0; i < n; i++)
+		free(s[i]);
+	free(s);
+	return 0;
+}
