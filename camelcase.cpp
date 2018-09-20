857d4eea9ddc5f7a5953221a1dba9c1fdbba3b00
Zavvio Mok <zavvio@gmail.com>
2018-09-20 14:44:11 -0700

    CamelCase

---
+++ camelcase.cpp
@@ -0,0 +1,31 @@
+// https://www.hackerrank.com/challenges/camelcase
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the camelcase function below.
+int camelcase(string s) {
+    int count = 1;
+    for (int i = 0; i < s.length(); i++) {
+        if (s[i] >= 'A' && s[i] <= 'Z') {
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    string s;
+    getline(cin, s);
+
+    int result = camelcase(s);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
