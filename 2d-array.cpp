db5f6db3a3fee732626267fa26d9358f71666e73
Zavvio Mok <zavvio@gmail.com>
2018-09-19 17:13:49 -0700

    2D Array - DS

---
+++ 2d-array.cpp
@@ -0,0 +1,43 @@
+// https://www.hackerrank.com/challenges/2d-array
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the hourglassSum function below.
+int hourglassSum(vector<vector<int>> arr) {
+    int sum;
+    int max = -999;
+    for (int i = 1; i < 5; i++) {
+        sum = 0;
+        for (int j = 1; j < 5; j++) {
+            sum = arr[i-1][j-1] + arr[i-1][j] + arr[i-1][j+1] + arr[i][j] + arr[i+1][j-1] + arr[i+1][j] + arr[i+1][j+1];
+            if (sum > max)
+                max = sum;
+        }
+    }
+    return max;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    vector<vector<int>> arr(6);
+    for (int i = 0; i < 6; i++) {
+        arr[i].resize(6);
+
+        for (int j = 0; j < 6; j++) {
+            cin >> arr[i][j];
+        }
+
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    }
+
+    int result = hourglassSum(arr);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
