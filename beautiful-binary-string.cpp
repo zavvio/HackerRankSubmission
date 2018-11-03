bfde2833d3561a0592e63b748ef96f847e89aafb
Zavvio Mok <zavvio@gmail.com>
2018-11-03 08:05:02 -0700

    Beautiful Binary String

---
+++ beautiful-binary-string.cpp
@@ -0,0 +1,36 @@
+// https://www.hackerrank.com/challenges/beautiful-binary-string
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the beautifulBinaryString function below.
+int beautifulBinaryString(string b) {
+    int count = 0;
+    for (int i = 0; i < b.length() - 2; i++) {
+        if (b[i] == '0' && b[i+1] == '1' && b[i+2] == '0') {
+            b[i+2] = 1;
+            count++;
+        }
+    }
+    return count;
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    string b;
+    getline(cin, b);
+
+    int result = beautifulBinaryString(b);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
